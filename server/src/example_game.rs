4c42aa4bfad83478a8f65169aa209724bff29524
Vidar Magnusson <git@vidarmagnusson.com>
2023-10-28 13:01:40 +0200

    Refactor example game

---
+++ server/src/example_game.rs
@@ -0,0 +1,107 @@
+use crate::{
+    data::{common::faction::Faction, components::strategy_card::StrategyCard},
+    game::{Event, Game},
+    player::Player,
+};
+
+pub fn play_example_game() -> Game {
+    let mut game = Game::new(vec![]);
+
+    let events = vec![
+        Event::AddPlayer {
+            player: Player {
+                name: "Tux".into(),
+                faction: Faction::SardakkNorr,
+                planets: vec![],
+            },
+        },
+        Event::AddPlayer {
+            player: Player {
+                name: "Vidde".into(),
+                faction: Faction::UniversitiesOfJolNar,
+                planets: vec![],
+            },
+        },
+        Event::AddPlayer {
+            player: Player {
+                name: "Gurr".into(),
+                faction: Faction::NekroVirus,
+                planets: vec![],
+            },
+        },
+        Event::StartGame,
+        Event::TakeStrategyCard {
+            player: "Tux".into(),
+            card: StrategyCard::Leadership,
+        },
+        Event::TakeStrategyCard {
+            player: "Vidde".into(),
+            card: StrategyCard::Technology,
+        },
+        Event::TakeStrategyCard {
+            player: "Gurr".into(),
+            card: StrategyCard::Diplomacy,
+        },
+        Event::TakeStrategyCard {
+            player: "Tux".into(),
+            card: StrategyCard::Construction,
+        },
+        Event::TakeStrategyCard {
+            player: "Vidde".into(),
+            card: StrategyCard::Imperial,
+        },
+        Event::TakeStrategyCard {
+            player: "Gurr".into(),
+            card: StrategyCard::Politics,
+        },
+        Event::CompleteStrategyPhase,
+        Event::TacticalAction {
+            player: "Tux".into(),
+        },
+        Event::StrategicActionBegin {
+            player: "Gurr".into(),
+            card: StrategyCard::Diplomacy,
+        },
+        Event::StrategicActionCommit,
+        Event::StrategicActionBegin {
+            player: "Vidde".into(),
+            card: StrategyCard::Technology,
+        },
+        Event::StrategicActionCommit,
+        Event::StrategicActionBegin {
+            player: "Tux".into(),
+            card: StrategyCard::Leadership,
+        },
+        Event::StrategicActionCommit,
+        // Event::StrategicActionBegin {
+        //     player: "Gurr".into(),
+        //     card: StrategyCard::Politics,
+        // },
+        // Event::StrategicActionCommit,
+        // Event::StrategicActionBegin {
+        //     player: "Vidde".into(),
+        //     card: StrategyCard::Imperial,
+        // },
+        // Event::StrategicActionCommit,
+        // Event::StrategicActionBegin {
+        //     player: "Tux".into(),
+        //     card: StrategyCard::Construction,
+        // },
+        // Event::StrategicActionCommit,
+        // Event::PassAction {
+        //     player: "Gurr".into(),
+        // },
+        // Event::PassAction {
+        //     player: "Vidde".into(),
+        // },
+        // Event::PassAction {
+        //     player: "Tux".into(),
+        // },
+    ];
+
+    for event in events {
+        game.apply(event);
+    }
+
+    game
+}
