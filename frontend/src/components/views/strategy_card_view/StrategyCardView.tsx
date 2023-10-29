e903107d493ae54b6abece704972ee0d81fa75b0
Vidar Magnusson <git@vidarmagnusson.com>
2023-10-29 22:44:23 +0100

    Update strategy card secondary view

--- frontend/src/components/views/strategy_card_view/StrategyCardView.tsx
+++ frontend/src/components/views/strategy_card_view/StrategyCardView.tsx
@@ -1,6 +1,7 @@
 import { GameState, PlayerId } from "@/api/Game";
 import { Button } from "@/components/elements/button/Button";
 import { FactionIcon } from "@/components/elements/factionIcon/FactionIcon";
+import styles from "./StrategyCardView.module.scss";
 
 export interface StrategyCardView {
   gameState: GameState;
@@ -22,35 +23,45 @@ export const StrategyCardView = ({
   };
 
   return (
-    <div className={`card`}>
+    <div className={`card ${styles.strategyCardView}`}>
       <h2>{gameState.strategicAction?.card}</h2>
-      <div>
-        {Object.keys(gameState.players)
-          .filter((p) => p !== gameState.currentPlayer)
-          .map((p) => gameState.players[p])
-          .map((p) => (
-            <div key={p.name}>
+      {Object.keys(gameState.players)
+        .filter((p) => p !== gameState.currentPlayer)
+        .map((p) => gameState.players[p])
+        .map((p) => (
+          <div key={p.name} className={styles.playerRow}>
+            <div>
               {p.name} <FactionIcon faction={p.faction} />
-              {Object.keys(strategicAction.otherPlayers).includes(p.name) ? (
-                <p>{strategicAction.otherPlayers[p.name] ? "V" : "X"}</p>
-              ) : (
-                <>
-                  <Button onClick={() => doAction(p.name, true)}>Play</Button>
-                  <Button onClick={() => doAction(p.name, false)}>Skip</Button>
-                </>
-              )}
             </div>
-          ))}
-        <Button
-          disabled={
-            Object.keys(strategicAction.otherPlayers).length <
-            Object.keys(gameState.players).length - 1
-          }
-          onClick={() => sendMessage("StrategicActionCommit")}
-        >
-          Submit
-        </Button>
-      </div>
+            {Object.keys(strategicAction.otherPlayers).includes(p.name) ? (
+              <div className={styles.actionResult}>
+                <p
+                  className={
+                    strategicAction.otherPlayers[p.name]
+                      ? styles.performed
+                      : styles.skipped
+                  }
+                >
+                  {strategicAction.otherPlayers[p.name] ? "V" : "X"}
+                </p>
+              </div>
+            ) : (
+              <div className={styles.buttonGroup}>
+                <Button onClick={() => doAction(p.name, true)}>Play</Button>
+                <Button onClick={() => doAction(p.name, false)}>Skip</Button>
+              </div>
+            )}
+          </div>
+        ))}
+      <Button
+        disabled={
+          Object.keys(strategicAction.otherPlayers).length <
+          Object.keys(gameState.players).length - 1
+        }
+        onClick={() => sendMessage("StrategicActionCommit")}
+      >
+        Submit
+      </Button>
     </div>
   );
 };
