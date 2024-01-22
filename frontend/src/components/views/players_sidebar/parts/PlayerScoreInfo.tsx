adf7c9827106e6523d644ff0479d053d337501a2
Vidar Magnusson <git@vidarmagnusson.com>
2024-01-22 22:08:54 +0100

    Remove outdated tooltip

--- frontend/src/components/views/players_sidebar/parts/PlayerScoreInfo.tsx
+++ frontend/src/components/views/players_sidebar/parts/PlayerScoreInfo.tsx
@@ -10,49 +10,12 @@ export interface PlayerScoreInfoProps {
 }
 
 export const PlayerScoreInfo = ({ player, score }: PlayerScoreInfoProps) => {
-  const { gameOptions } = useGameContext();
-
   const isCustodian = score?.custodians === player.name;
   const currentScore = score.playerPoints[player.name];
-  const publicObjectivePoints = Object.keys(score.revealedObjectives)
-    .map((obj) => {
-      return {
-        obj: obj,
-        objInfo: gameOptions.objectives[obj],
-        players: score.revealedObjectives[obj],
-      };
-    })
-    .filter((obj) => obj.players.includes(player.name))
-    .map((obj) => {
-      if (obj.objInfo.kind === "StageII") {
-        return 2;
-      } else {
-        return 1;
-      }
-    })
-    .reduce((acc, curr) => acc + curr, 0);
-
-  const playerObjectives = score.secretObjectives[player.name];
-  const secretObjectivePoints = playerObjectives ? playerObjectives.length : 0;
-  const supportForTheThronePoints = Object.values(
-    score.supportForTheThrone
-  ).filter((id) => id === player.name).length;
-  const imperialPoints = score.imperial[player.name] ?? 0;
-  const extraPoints = score.extraPoints[player.name] ?? 0;
-
-  const scoreTooltip = `Score ${currentScore} due to:
- - Custodians (${isCustodian ? "1" : "0"})
- - Public objectives (${publicObjectivePoints})
- - Secret objectives (${secretObjectivePoints})
- - Support ft Throne (${supportForTheThronePoints})
- - Imperial (${imperialPoints})
- - Extra (${extraPoints})`;
 
   return (
     <div className={styles.scoreContainer}>
-      <h3 className={styles.scoreText} title={scoreTooltip}>
-        {currentScore}
-      </h3>
+      <h3 className={styles.scoreText}>{currentScore}</h3>
 
       {isCustodian && (
         <div>
