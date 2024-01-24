d9c5add48f2886e6cca8626b02a5d3cf602870fe
Vidar Magnusson <vmagnus3@volvocars.com>
2024-01-24 09:28:52 +0100

    More styling changes

--- frontend/src/components/views/strategy_card_view/secondary_views/TechSecondary.tsx
+++ frontend/src/components/views/strategy_card_view/secondary_views/TechSecondary.tsx
@@ -29,49 +29,52 @@ export const StrategyTechnologySecondaryView = () => {
     .sort(nameSort);
 
   return (
-    <div>
+    <div className={`column ${styles.genericSecondaryContainer}`}>
       {players.map((p) => {
-        if (donePlayers[p.id]) {
-          const choice = donePlayers[p.id] as
-            | "Skipped"
-            | {
-                Technology: {
-                  tech: string;
-                };
-              };
-          return (
-            <div key={p.id}>
-              {p.name} <FactionIcon faction={p.faction} />
-              {choice === "Skipped" ? (
-                <p>--Skipped--</p>
-              ) : (
-                <p>
-                  Tech: {gameOptions.technologies[choice.Technology.tech].name}
-                </p>
-              )}
-            </div>
-          );
-        } else {
-          return (
-            <fieldset key={p.id}>
-              <legend className={styles.alignedLegend}>
-                <h6 className={styles.horizontalPadding}>{p.name}</h6>
-                <FactionIcon faction={p.faction} />
-              </legend>
-              <SelectTechView
-                playerId={p.name}
-                onSelect={(tech) =>
-                  sendTechSecondaryMessage(p.name, {
-                    Technology: { tech: tech },
-                  })
-                }
-              />
-              <Button onClick={() => sendTechSecondaryMessage(p.name, "Skip")}>
-                Skip
-              </Button>
-            </fieldset>
-          );
-        }
+        const choice = donePlayers[p.id] as
+          | "Skipped"
+          | { Technology: { tech: string } }
+          | undefined;
+
+        return (
+          <fieldset key={p.id} className={styles.techSecondaryFieldset}>
+            <legend className={styles.alignedLegend}>
+              <h6 className={styles.horizontalPadding}>{p.name}</h6>
+              <FactionIcon faction={p.faction} />
+            </legend>
+            {choice === undefined ? (
+              <>
+                <SelectTechView
+                  playerId={p.name}
+                  onSelect={(tech) =>
+                    sendTechSecondaryMessage(p.name, {
+                      Technology: { tech: tech },
+                    })
+                  }
+                />
+                <div className={styles.skipDivider} />
+                <div className={styles.techSkipButton}>
+                  <Button
+                    onClick={() => sendTechSecondaryMessage(p.name, "Skip")}
+                  >
+                    Skip
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <>
+                {choice === "Skipped" ? (
+                  <p>--Skipped--</p>
+                ) : (
+                  <p>
+                    Tech:{" "}
+                    {gameOptions.technologies[choice.Technology.tech].name}
+                  </p>
+                )}
+              </>
+            )}
+          </fieldset>
+        );
       })}
     </div>
   );
