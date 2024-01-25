d7591e03136fbce404473f3a93fddccabb0599ff
Vidar Magnusson <git@vidarmagnusson.com>
2024-01-25 20:39:52 +0100

    Show viewing as in top card

--- frontend/src/components/views/game/GameView.tsx
+++ frontend/src/components/views/game/GameView.tsx
@@ -212,6 +212,7 @@ export const GameView = ({ gameId, wsUri }: GameViewProps) => {
         </div>
         <p className="marginTop">Round {gameState?.round}</p>
         {currentPlayer && <p>Current player: {currentPlayer.name}</p>}
+        Currently viewing: {playingAs ?? "Global"}
         <div>
           <Button
             className="marginRight"
