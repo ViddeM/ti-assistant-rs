ff811b8db81a798b38f2e5e4fb21d2c932a27866
Vidar Magnusson <git@vidarmagnusson.com>
2024-04-17 18:25:18 +0200

    Update infobutton props

--- frontend/src/components/elements/button/InfoButton.tsx
+++ frontend/src/components/elements/button/InfoButton.tsx
@@ -3,16 +3,24 @@ import { useGameContext } from "@/hooks/GameContext";
 import { faInfo } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Button.module.scss";
+import { ButtonHTMLAttributes, FC } from "react";
+import { ButtonBaseProps } from "./Button";
 
-export interface InfoButtonProps {
+export type InfoButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   info: InfoObject;
-}
+};
 
-export const InfoButton = ({ info }: InfoButtonProps) => {
+export const InfoButton: FC<InfoButtonProps> = ({
+  info,
+  className,
+  ...props
+}) => {
   const { showInfo } = useGameContext();
 
+  const style = `${className} ${styles.infoButton}`;
+
   return (
-    <button onClick={() => showInfo(info)} className={styles.infoButton}>
+    <button onClick={() => showInfo(info)} className={style} {...props}>
       <FontAwesomeIcon icon={faInfo} />
     </button>
   );
