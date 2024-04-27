a8e776919ee69056ccb283c22514d67fdc622f2b
Vidar Magnusson <git@vidarmagnusson.com>
2024-04-27 09:57:37 +0200

    TEMP: BEGAN REWORK OF MAIN MENU

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,37 +1,41 @@
 "use client";
 
 import Link from "next/link";
-import styles from "./page.module.scss";
 import { useState } from "react";
-import { Button } from "@/components/elements/button/Button";
-import { useRouter } from "next/navigation";
+import styles from "./page.module.scss";
 
 export default function Home() {
   const [gameId, setGameId] = useState<string>("");
-  const router = useRouter();
 
   return (
     <div className="card">
-      <h1>TI-Assistant</h1>
-      <ul className={styles.gameLinks}>
-        <li>
-          <Link href={"/game/new"}>New Game</Link>
-        </li>
-        <li>
-          <Link href={"/game/c561033b"}>Three Player Demo</Link>
-        </li>
-        <li>
-          <Link href={"/game/7d96c760"}>Five Player Demo</Link>
-        </li>
-      </ul>
+      <h1>TI-Assistant (RS)</h1>
+
+      <div className={styles.newGameContainer}>
+        <Link href={"/game/new"} className={styles.newGameLink}>
+          <h2>New Game</h2>
+        </Link>
+      </div>
+
       <div>
         <label htmlFor="join_game_input">Game ID: </label>
         <input
+          id="join_game_input"
+          className={styles.gameIdInput}
           value={gameId}
           onChange={(e) => setGameId(e.target.value ?? "")}
+          maxLength={8}
         />
       </div>
-      <Button onClick={() => router.push(`/game/${gameId}`)}>Join game</Button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Link href={`/game/${gameId}`}>Join Game</Link>
+      </div>
     </div>
   );
 }
