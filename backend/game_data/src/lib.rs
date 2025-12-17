5080b8da04dedef72fafd033a9ca62bbba2eeb98
Vidar Magnusson <git@vidarmagnusson.com>
2025-12-17 16:48:21 +0100

    Increase recursion limit to fix planet compile issue

--- backend/game_data/src/lib.rs
+++ backend/game_data/src/lib.rs
@@ -1,3 +1,6 @@
+// TODO: Try to lower this when we no longer need TS.
+#![recursion_limit = "256"]
+
 /// HashMap wrapper type for generating typescript bindings.
 pub mod enum_map;
 // Common types.
