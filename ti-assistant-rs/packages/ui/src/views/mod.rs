566fe6702cd6fd80c91bf29b9c9fa78eca0b1074
Vidar Magnusson <git@vidarmagnusson.com>
2025-12-25 12:58:28 +0100

    Main menu

--- ti-assistant-rs/packages/ui/src/views/mod.rs
+++ ti-assistant-rs/packages/ui/src/views/mod.rs
@@ -1,4 +1,5 @@
 pub mod game;
 pub mod info_box;
+pub mod main_menu;
 pub mod phase_view;
 pub mod phase_views;
--- ti-assistant-rs/packages/web/src/main_menu.rs
+++ ti-assistant-rs/packages/web/src/main_menu.rs
@@ -1,32 +1,14 @@
 use dioxus::prelude::*;
-use ui::components::button::Button;
+use ui::views;
 
 use crate::Route;
 
 #[component]
 pub fn MainMenu() -> Element {
-    let mut game_id = use_signal(|| "".to_string());
-
     rsx! {
-        div {
-            Link { to: Route::NewGame,
-                Button { "New Game" }
-            }
-
-            input {
-                placeholder: "Game ID",
-                value: game_id(),
-                oninput: move |evt| {
-                    let value = evt.value();
-                    let prev = game_id();
-                    let value = if value.chars().count() > 8 { prev } else { value };
-                    game_id.set(value);
-                },
-            }
-
-            Link { to: Route::Game { id: game_id() },
-                Button { disabled: game_id().len() != 8, "Join Game" }
-            }
+        views::main_menu::MainMenu {
+            new_game: || Route::NewGame,
+            join_game: |game_id| Route::Game { id: game_id },
         }
     }
 }
