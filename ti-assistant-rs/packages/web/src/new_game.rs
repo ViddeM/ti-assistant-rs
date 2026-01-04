d8e11b5091f3bfa80849682cce0f0aa94643c335
Vidar Magnusson <git@vidarmagnusson.com>
2026-01-04 13:31:58 +0100

    Work on rework new game screen

--- ti-assistant-rs/packages/web/src/new_game.rs
+++ ti-assistant-rs/packages/web/src/new_game.rs
@@ -1,6 +1,7 @@
 use dioxus::prelude::*;
 use ui::{
     components::button::Button,
+    data::game_context::GameContext,
     endpoints,
     game_id::GameId,
     requests::new_game::{self, GameConfig},
@@ -16,17 +17,21 @@ enum CreateGameMode {
 
 #[component]
 pub fn NewGame() -> Element {
+    let event = use_context::<GameContext>();
     let mut winning_score = use_signal(|| 10);
     let mut mode = use_signal(|| CreateGameMode::New);
-    let mut pok = use_signal(|| false);
-    let mut te = use_signal(|| false);
-    let mut codexI = use_signal(|| false);
-    let mut codexII = use_signal(|| false);
-    let mut codexIII = use_signal(|| false);
-
     let nav = navigator();
 
     let mut new_game_result: Signal<Option<Result<GameId, ServerFnError>>> = use_signal(|| None);
+
+    let view_mode = use_memo(move || match *mode.read() {
+        CreateGameMode::New => rsx! {
+            CleanNewGameView {}
+        },
+        CreateGameMode::MiltyImport => rsx! {
+            ImportMiltyGame {}
+        },
+    });
 
     match &new_game_result() {
         Some(Err(err)) => rsx! {
@@ -48,85 +53,108 @@ pub fn NewGame() -> Element {
         }
         None => {
             rsx! {
-                div { class: "card create-game-container" }
-
-                div {
+                div { class: "card create-game-container",
+                    h2 { "Create Game" }
                     label { r#for: "winning_score", "Winning Score" }
-                    input {
-                        r#type: "range",
-                        name: "winning_score",
-                        min: 4,
-                        max: 16,
-                        value: winning_score,
-                        oninput: move |event| *winning_score.write() = event.value().parse().unwrap(),
-                    }
-                    p { "{winning_score}" }
-
-                    Button {
-                        disabled: mode.read().eq(&CreateGameMode::New),
-                        onclick: move |_| *mode.write() = CreateGameMode::New,
-                        "New Game"
-                    }
-                    Button {
-                        disabled: mode.read().eq(&CreateGameMode::MiltyImport),
-                        onclick: move |_| *mode.write() = CreateGameMode::MiltyImport,
-                        "Import from Milty"
+                    div { class: "create-game-row center-row",
+                        input {
+                            r#type: "range",
+                            name: "winning_score",
+                            min: 4,
+                            max: 16,
+                            value: winning_score,
+                            oninput: move |event| *winning_score.write() = event.value().parse().unwrap(),
+                        }
+                        p { "{winning_score}" }
                     }
 
-                    label { r#for: "pok_cb", "Prophecy of Kings" }
-                    input {
-                        r#type: "checkbox",
-                        name: "pok_cb",
-                        onchange: move |_| pok.toggle(),
+                    div { class: "view-mode-button-group margin-bottom",
+                        Button {
+                            disabled: mode.read().eq(&CreateGameMode::New),
+                            onclick: move |_| *mode.write() = CreateGameMode::New,
+                            "New Game"
+                        }
+                        Button {
+                            disabled: mode.read().eq(&CreateGameMode::MiltyImport),
+                            onclick: move |_| *mode.write() = CreateGameMode::MiltyImport,
+                            "Import from Milty"
+                        }
                     }
 
-                    label { r#for: "pok_codI_cb", "Codex I" }
-                    input {
-                        r#type: "checkbox",
-                        name: "pok_codI_cb",
-                        onchange: move |_| codexI.toggle(),
-                    }
-
-                    label { r#for: "pok_codII_cb", "Codex II" }
-                    input {
-                        r#type: "checkbox",
-                        name: "pok_codII_cb",
-                        onchange: move |_| codexII.toggle(),
-                    }
-
-                    label { r#for: "pok_codIII_cb", "Codex III" }
-                    input {
-                        r#type: "checkbox",
-                        name: "pok_codIII_cb",
-                        onchange: move |_| codexIII.toggle(),
-                    }
-
-                    label { r#for: "te_cb", "Thunder's Edge" }
-                    input {
-                        r#type: "checkbox",
-                        name: "te_cb",
-                        onchange: move |_| te.toggle(),
-                    }
-
-                    Button {
-                        onclick: move |_| async move {
-                            let ngr = endpoints::new_game(new_game::NewGame {
-                                    points: winning_score(),
-                                    game_config: GameConfig::CustomGameConfig {
-                                        pok: pok(),
-                                        cod1: codexI(),
-                                        cod2: codexII(),
-                                        cod3: codexIII(),
-                                        te: te(),
-                                    },
-                                })
-                                .await;
-                            *new_game_result.write() = Some(ngr);
-                        },
-                        "Create Game"
-                    }
+                    {view_mode}
                 }
             }
         }
     }
+}
+
+#[component]
+fn CleanNewGameView() -> Element {
+    let mut pok = use_signal(|| false);
+    let mut te = use_signal(|| false);
+    let mut codexI = use_signal(|| false);
+    let mut codexII = use_signal(|| false);
+    let mut codexIII = use_signal(|| false);
+
+    rsx! {
+
+        label { r#for: "pok_cb", "Prophecy of Kings" }
+        input {
+            r#type: "checkbox",
+            name: "pok_cb",
+            onchange: move |_| pok.toggle(),
+        }
+
+        label { r#for: "pok_codI_cb", "Codex I" }
+        input {
+            r#type: "checkbox",
+            name: "pok_codI_cb",
+            onchange: move |_| codexI.toggle(),
+        }
+
+        label { r#for: "pok_codII_cb", "Codex II" }
+        input {
+            r#type: "checkbox",
+            name: "pok_codII_cb",
+            onchange: move |_| codexII.toggle(),
+        }
+
+        label { r#for: "pok_codIII_cb", "Codex III" }
+        input {
+            r#type: "checkbox",
+            name: "pok_codIII_cb",
+            onchange: move |_| codexIII.toggle(),
+        }
+
+        label { r#for: "te_cb", "Thunder's Edge" }
+        input {
+            r#type: "checkbox",
+            name: "te_cb",
+            onchange: move |_| te.toggle(),
+        }
+
+        Button {
+            onclick: move |_| async move {
+                let ngr = endpoints::new_game(new_game::NewGame {
+                        points: winning_score(),
+                        game_config: GameConfig::CustomGameConfig {
+                            pok: pok(),
+                            cod1: codexI(),
+                            cod2: codexII(),
+                            cod3: codexIII(),
+                            te: te(),
+                        },
+                    })
+                    .await;
+                *new_game_result.write() = Some(ngr);
+            },
+            "Create Game"
+        }
+
+    }
+}
+
+#[component]
+fn ImportMiltyGame() -> Element {
+    rsx! {}
 }
